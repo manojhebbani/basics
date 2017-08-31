fad4d42f15c7f3aa3a53b89460e905d9a156181f
manojhebbani <manojkumar.hv@abfrl.adityabirla.com>
2017-08-31 10:58:03 +0530

    Fixes

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.set('view engine', 'hbs');
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
-
   console.log(log);
   fs.appendFile('server.log', log + '\n');
   next();

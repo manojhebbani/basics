445a81fc0d88816312e3f64b655d97b0cc8445dd
manojhebbani <hvmanojk@gmail.com>
2017-10-28 18:10:42 +0530

    checks

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
-var app = express();
+var appp = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 
